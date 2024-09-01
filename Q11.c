c52c1038c33ea8d6f42cab173a93fdf54b0fd379
acelaria <arushiace17@gmail.com>
2024-09-01 16:37:28 +0530

    Q11

--- Q11.c
+++ Q11.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 int main(){
-	int ts,h,m,s;
+	int ts,h,m,s,remainingseconds;
+    printf("PROGRAM TO READ TOTAL SECOND AND CONVERT IT INTO TIME\n");
 	printf("enter total seconds:");
 	scanf("%d",&ts);
-	h = ts*3600;
-	m = ts*60;
-	s = ts;
-	printf("h:%d\n m:%d\n s:%d\n",h,m,s);
-	return 0;
+    h = ts/3600;
+    remainingseconds = ts % 3600;
+    m = remainingseconds / 60;
+    remainingseconds = remainingseconds % 60;
+    s = remainingseconds;
+    printf("D:H:M - %d:%d:%d\n",h,m,s);
+	
+    return 0;
+
 }
