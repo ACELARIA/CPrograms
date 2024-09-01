ad055ea4fac0d7161ab99b4f6ebaf7350864a941
acelaria <arushiace17@gmail.com>
2024-09-01 16:00:04 +0530

    Q17

---
+++ Q17.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int num1, num2, num3;
+    printf("PROGRAM TO READ THREE NOS. AND PRINT MAX USING LOGICAL OPERATOR.(&&)\n");
+    printf("Enter 3 numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    int max = num1;
+    if (num2 > max && num2 > num3) 
+    {
+        max = num2;
+    } else if (num3 > max) 
+    {
+        max = num3;
+    }
+
+    printf("The maximum number is: %d\n", max);
+
+    return 0;
+}
