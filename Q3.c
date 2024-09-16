99686351bb2ec7be8967f5752619b5783caae05b
acelaria <arushiace17@gmail.com>
2024-09-16 20:55:49 +0530

    Q3

--- Q3.c
+++ Q3.c
@@ -2,10 +2,13 @@
 int main()
 {
 	int gs,bs,da,ta;
+	printf("PROGRAM TO CALCULATE GROSS SALARY\n");
 	printf("enter basic salary:");
 	scanf("%d",&bs);
 	da=(10*bs)/100;
+	printf("da=%d", da);
 	ta=(12*bs)/100;
+	printf("\nta=%d", ta);
 	gs=bs+da+ta;
 	printf("\ngross salary = %d\n",gs);
 	return 0;
