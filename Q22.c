0eb18628f6748d3e48f51a8ac4903cf18ac596f1
acelaria <arushiace17@gmail.com>
2024-09-16 22:01:01 +0530

    Q39

---
+++ Q22.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num,i,fact = 1;
+    printf("enter number: ");
+    scanf("%d",&num)
+    for( i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
--- Q39.c
+++ Q39.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
-int main() {
-    int n, i, j;
-    float term, sum;
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
 
-    printf("Enter the value of N: ");
+int main() {
+    int n, term;
+    double sum = 0.0;
+
+    printf("Enter the number of terms: ");
     scanf("%d", &n);
 
-    sum = 0;
-    for (i = 1; i <= n; i++) {
-        term = 1;
-        for (j = 1; j <= i; j++) {
-            term *= j;
-        }
-        sum += (float)i / term;
+    for (int i = 1; i <= n; i++) {
+        term = i / factorial(i);
+        sum += term;
     }
 
-    printf("The sum of the series is: %.2f\n", sum);
+    printf("Sum of the series: %.2f\n", sum);
 
     return 0;
 }
