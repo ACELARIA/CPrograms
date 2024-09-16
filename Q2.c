4232f6f42cde3f30c2566ceb6fb84d081e802a2f
acelaria <arushiace17@gmail.com>
2024-09-16 20:46:10 +0530

    Q2

--- Q2.c
+++ Q2.c
@@ -3,12 +3,22 @@ int main()
 {
 	int s1,s2,s3,s4,s5,sum,total=500;
 	float per;
-	printf("enter marks of 5 subjects");
-	scanf("%d%d%d%d%d",&s1,&s2,&s3,&s4,&s5);
+	printf("PROGRAM TO READ MARKS OF FIVE SUBJECT OF A STUDENT AND CALCULATE TOTAL AND PERCENTAGE\n");
+	printf("enter marks of subject 1: ");
+	scanf("%d",&s1);
+	printf("enter marks of subject 2: ");
+	scanf("%d",&s2);
+	printf("enter marks of subject 3: ");
+	scanf("%d",&s3);
+	printf("enter marks of subject 4: ");
+	scanf("%d",&s4);
+	printf("enter marks of subject 5: ");
+	scanf("%d",&s5);
+
 	sum=s1+s2+s3+s4+s5;
-	printf("sum=%d",sum);
+	printf("sum= %d",sum);
 	per=(sum*100)/total;
-	printf("\npercentage=%f\n",per);
+	printf("\npercentage= %.2f\n",per);
 	return 0;
 }
 
