8358d05603820957d0197431e120302b73eb9448
acelaria <arushiace17@gmail.com>
2024-09-19 14:38:13 +0530

    Q53

---
+++ Q54.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[10];
+    int sum = 0;
+
+    printf("Enter 10 integers:\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("Number %d: ", i + 1);
+        scanf("%d", &numbers[i]);
+        sum += numbers[i];
+    }
+
+    printf("Sum of the numbers: %d\n", sum);
+
+    return 0;
+}
