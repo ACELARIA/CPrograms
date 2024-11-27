36d5d435110d43f3c8a4804f9a7703444f1c7733
acelaria <arushiace17@gmail.com>
2024-11-27 22:09:22 +0530

    Q46.c

--- Q46.c
+++ Q46.c
@@ -6,7 +6,7 @@ int main() {
     printf("Enter your head circumference in inches: ");
     scanf("%f", &head_circumference);
 
-    float hat_size = 6 + (head_circumference - 20) * 0.5;
+    float hat_size = 6 + (head_circumference - 20) * 0.125;
     printf("Your estimated hat size is: %.1f\n", hat_size);
 
 }
